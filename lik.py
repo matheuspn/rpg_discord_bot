d03f4eb003b54f0c1b459928ae14d695d95a2e7e
matheuspn <mat.procopip69@gmail.com>
2021-03-17 15:58:35 -0300

    help commands

--- lik.py
+++ lik.py
@@ -3,11 +3,20 @@ from discord.ext import commands
 
 import psycopg3
 from random import choice
+import logging
 
-# db connect
+# loggin reports
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+# db access information
 with open("db_access.txt", "r") as f:
     db = f.read()
 
+# db connection
 try:
     conn = psycopg3.connect(db)
     cur = conn.cursor()
@@ -32,7 +41,7 @@ def atribui_status():
 
     return status
 
-bot = commands.Bot(command_prefix= '>')
+bot = commands.Bot(command_prefix= '>', help_command= None)
 
 @bot.event
 async def on_ready():
@@ -41,8 +50,30 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
+@bot.group(invoke_without_command= True)
+async def help(ctx):
+    em = discord.Embed(title= "Help", description= "Use >help 'command' para detalhes do comando.")
+
+    em.add_field(name= "Commands", value= "entrar, soldado")
+
+    await ctx.send(embed= em)
+
+@help.command()
+async def soldado(ctx):
+    em = discord.Embed(title= "Soldado", description= "Compra um soldado com status aleatórios")
+
+    em.add_field(name= "Como usar", value= ">soldado 'nome do soldado'")
+
+    await ctx.send(embed= em)
+
+@help.command()
 async def entrar(ctx):
+    em = discord.Embed(title= "Entrar", description= "Entra no jogo e recebe 200 moedas")
+
+    await ctx.send(embed= em)
+
+@bot.command(name= "entrar")
+async def _entrar(ctx):
     try:
         cur.execute(f"SELECT to_regclass('public.id_{ctx.author.id}')")
         result = cur.fetchall()
@@ -59,8 +90,8 @@ async def entrar(ctx):
         await ctx.send("Algo deu errado tente novamente mais tarde e/ou avisa os moderadores.")
         print(EnvironmentError)
 
-@bot.command()
-async def soldado(ctx, nome):
+@bot.command(name= "soldado")
+async def _soldado(ctx, nome):
     try:
         name = ('gold', )
         cur.execute(f"SELECT moedas FROM id_{ctx.author.id} WHERE nome = %s", ('gold', ))
@@ -73,14 +104,14 @@ async def soldado(ctx, nome):
             status = atribui_status()
             cur.execute(f"INSERT INTO id_{ctx.author.id} VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s)", (nome, status["forca"], status["armadura"], status["vida"], status["vel_ataque"], 1, 0))
             conn.commit()
-            await ctx.send(f"@{ctx.author.name} você adiquiriu um soldado com os seguintes status:\nForça: {status['forca']}\nArmadura: {status['armadura']}\nVida: {status['vida']}\nVel. Ataque: {status['vel_ataque']}")
+            await ctx.send(f"{ctx.author.name} você adiquiriu um soldado com os seguintes status:\nForça: {status['forca']}\nArmadura: {status['armadura']}\nVida: {status['vida']}\nVel. Ataque: {status['vel_ataque']}")
         else:
-            await ctx.send(f"@{ctx.author.name} você precisa de 100 moedas para comprar um soldado, você possui {moedas}.")
+            await ctx.send(f"{ctx.author.name} você precisa de 100 moedas para comprar um soldado, você possui {moedas}.")
     except EnvironmentError:
-        await ctx.send(f"@{ctx.author.name} você dar um nome ao seu soldado.")
+        await ctx.send(f"{ctx.author.name} você dar um nome ao seu soldado.")
         print("Erro no bd",EnvironmentError)
 
-    
+
 
 
 with open("token.txt","r") as f:
